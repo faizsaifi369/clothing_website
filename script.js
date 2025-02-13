03a4fb7237fc4b558095e6b26cc8ed32b13b3218
Faiz saifi <sfaizsaifi7231@gmail.com>
2025-02-13 19:04:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+let morebtn = document.querySelector(".moreButton");
+// morebtn.style.backgroundColor="red";
+morebtn.addEventListener("click",() =>{
+    console.log("More button was clicked");
+});
